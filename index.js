9f6b8b9407a0e9618cf74f101133dc730df2779c
Ashan Dimantha <ashandimanthalk@gmail.com>
2024-04-16 20:19:53 +0530

    added:create

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 import express from "express";
-import mongoose from "mongoose";
 import { PORT, MONGO_URI } from "./.env";
+import mongoose from "mongoose";
+import cors from "cors";
+import contactRoute from "./routes/contactRoute.js";
+
 
 const app = express();
 
+//middleware to parse json data
+app.use(express.json());
+
+//middleware to handle CORS policy
+app.use(cors());
+
+//Get request to the root of the application
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
+//route for the contact details
+app.use("/contacts", contactRoute);
 
+
+//connect to the database and listen to the port
 mongoose
   .connect(MONGO_URI)
     .then(() => {
